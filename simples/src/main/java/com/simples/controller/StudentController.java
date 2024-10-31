5890a829b3e2176392fe376822604592cb2362ab
Abderrahman Badi <144786380+Yorften@users.noreply.github.com>
2024-10-31 21:20:01 +0100

    Students pagination done

--- simples/src/main/java/com/simples/controller/StudentController.java
+++ simples/src/main/java/com/simples/controller/StudentController.java
@@ -1,10 +1,12 @@
 package com.simples.controller;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.simples.dto.StudentDTO;
@@ -34,94 +37,102 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 public class StudentController {
 
         @Autowired
-    private StudentService studentService;
+        private StudentService studentService;
 
-    /**
-     * Handles POST requests to save a new student.
-     * 
-     * @param student the student entity to be saved
-     * @return the saved student entity
-     */
-    @Operation(summary = "Create a new student", description = "Saves a new student entity to the system.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "201", description = "Student created successfully"),
-            @ApiResponse(responseCode = "400", description = "Invalid input data")
-    })
-    @PostMapping
-    public StudentDTO saveStudent(@Valid @RequestBody Student student) {
-        return studentService.addStudent(student);
-    }
+        /**
+         * Handles POST requests to save a new student.
+         * 
+         * @param student the student entity to be saved
+         * @return the saved student entity
+         */
+        @Operation(summary = "Create a new student", description = "Saves a new student entity to the system.")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "201", description = "Student created successfully"),
+                        @ApiResponse(responseCode = "400", description = "Invalid input data")
+        })
+        @PostMapping
+        public StudentDTO saveStudent(@Valid @RequestBody Student student) {
+                return studentService.addStudent(student);
+        }
 
-    /**
-     * Handles GET requests to fetch the list of all students.
-     * 
-     * @return a list of student entities
-     */
-    @Operation(summary = "Get all students", description = "Fetches a list of all students in the system.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of students"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
-    })
-    @GetMapping
-    public List<StudentDTO> fetchStudentList() throws InvalidDataException {
-        return studentService.getStudentList();
-    }
+        /**
+         * Handles GET requests to fetch the list of all students.
+         * 
+         * @return a list of student entities
+         */
+        @Operation(summary = "Get all students", description = "Fetches a list of all students in the system.")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Successfully retrieved list of students"),
+                        @ApiResponse(responseCode = "500", description = "Internal server error")
+        })
+        @GetMapping
+        public Page<StudentDTO> fetchStudentList(
+                        @Parameter(description = "Page number") @RequestParam int page,
+                        @Parameter(description = "Size of items per page") @RequestParam int size)
+                        throws InvalidDataException {
+                Pageable pageable = PageRequest.of(page, size, Sort.by("id").ascending());
+                return studentService.getStudentList(pageable);
+        }
 
-    /**
-     * Handles GET requests to fetch the list of all students.
-     * 
-     * @return a list of student entities
-     */
-    @Operation(summary = "Get a student by ID", description = "Fetches a student entity by its ID.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found the student"),
-            @ApiResponse(responseCode = "400", description = "Invalid input data"),
-            @ApiResponse(responseCode = "404", description = "Student not found"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
-    })
-    @GetMapping("/{id}")
-    public StudentDTO getStudent(
-            @Parameter(description = "ID of the student to be retrieved") @PathVariable("id") Long studentId) throws ResourceNotFoundException, InvalidDataException {
-        return studentService.findStudentById(studentId, "classroom");
-    }
+        /**
+         * Handles GET requests to fetch the list of all students.
+         * 
+         * @return a list of student entities
+         */
+        @Operation(summary = "Get a student by ID", description = "Fetches a student entity by its ID.")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Found the student"),
+                        @ApiResponse(responseCode = "400", description = "Invalid input data"),
+                        @ApiResponse(responseCode = "404", description = "Student not found"),
+                        @ApiResponse(responseCode = "500", description = "Internal server error")
+        })
+        @GetMapping("/{id}")
+        public StudentDTO getStudent(
+                        @Parameter(description = "ID of the student to be retrieved") @PathVariable("id") Long studentId)
+                        throws ResourceNotFoundException, InvalidDataException {
+                return studentService.findStudentById(studentId, "classroom");
+        }
 
-    /**
-     * Handles PUT requests to update an existing student.
-     * 
-     * @param student   the student entity with updated information
-     * @param studentId the ID of the student to be updated
-     * @return the updated student entity
-     */
-    @Operation(summary = "Update an existing student", description = "Updates a student entity identified by its ID.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Student updated successfully"),
-            @ApiResponse(responseCode = "400", description = "Invalid input data"),
-            @ApiResponse(responseCode = "404", description = "Student not found"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
-    })
-    @PutMapping("/{id}")
-    public StudentDTO updateStudent(
-            @Parameter(description = "Updated student data") @RequestBody Student student,
-            @Parameter(description = "ID of the student to be updated") @PathVariable("id") Long studentId) throws ResourceNotFoundException {
-        return studentService.updateStudent(student, studentId);
-    }
+        /**
+         * Handles PUT requests to update an existing student.
+         * 
+         * @param student   the student entity with updated information
+         * @param studentId the ID of the student to be updated
+         * @return the updated student entity
+         */
+        @Operation(summary = "Update an existing student", description = "Updates a student entity identified by its ID.")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Student updated successfully"),
+                        @ApiResponse(responseCode = "400", description = "Invalid input data"),
+                        @ApiResponse(responseCode = "404", description = "Student not found"),
+                        @ApiResponse(responseCode = "500", description = "Internal server error")
+        })
+        @PutMapping("/{id}")
+        public StudentDTO updateStudent(
+                        @Parameter(description = "Updated student data") @RequestBody Student student,
+                        @Parameter(description = "ID of the student to be updated") @PathVariable("id") Long studentId)
+                        throws ResourceNotFoundException {
+                return studentService.updateStudent(student, studentId);
+        }
 
-    /**
-     * Handles DELETE requests to remove a student by ID.
-     * 
-     * @param studentId the ID of the student to be deleted
-     * @return a success message
-     */
-    @Operation(summary = "Delete a student by ID", description = "Deletes a student entity identified by its ID.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Student deleted successfully"),
-            @ApiResponse(responseCode = "404", description = "Student not found"),
-            @ApiResponse(responseCode = "500", description = "Internal server error")
-    })
-    @DeleteMapping("/{id}")
-    public String deleteStudentById(@Parameter(description = "ID of the student to be deleted") @PathVariable("id") Long studentId) throws ResourceNotFoundException {
-        studentService.deleteStudentById(studentId);
-        return "Deleted Successfully";
-    }
+        /**
+         * Handles DELETE requests to remove a student by ID.
+         * 
+         * @param studentId the ID of the student to be deleted
+         * @return a success message
+         */
+        @Operation(summary = "Delete a student by ID", description = "Deletes a student entity identified by its ID.")
+        @ApiResponses(value = {
+                        @ApiResponse(responseCode = "200", description = "Student deleted successfully"),
+                        @ApiResponse(responseCode = "404", description = "Student not found"),
+                        @ApiResponse(responseCode = "500", description = "Internal server error")
+        })
+        @DeleteMapping("/{id}")
+        public String deleteStudentById(
+                        @Parameter(description = "ID of the student to be deleted") @PathVariable("id") Long studentId)
+                        throws ResourceNotFoundException {
+                studentService.deleteStudentById(studentId);
+                return "Deleted Successfully";
+        }
 
 }
